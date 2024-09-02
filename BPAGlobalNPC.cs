fcd73a44df26c140f67cddedf50e9dfd9c5ffc02
First Fractal <firstfractal512@gmail.com>
2024-09-02 17:38:01 -0500

    created the dust radius

--- BPAGlobalNPC.cs
+++ BPAGlobalNPC.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Xna.Framework;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
 using Terraria;
-using Terraria.ModLoader;
 using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace BossesPoisonAura
 {
     internal class BPAGlobalNPC : GlobalNPC
     {
+        //the radius of the poison aura is 30 blocks (16 pixels in one block)
+        public static int radius = 30 * 16;
         public override void AI(NPC npc)
         {
             //check if the current npc is a boss
@@ -24,7 +21,7 @@ namespace BossesPoisonAura
                     if (player.active && !player.dead)
                     {
                         //check if the player is close enough to the boss
-                        if (Vector2.Distance(player.Center, npc.Center) < 30 * 16)
+                        if (Vector2.Distance(player.Center, npc.Center) < radius)
                         {
                             //give the player the buff
                             player.AddBuff(BuffID.Poisoned, 1);
@@ -34,6 +31,31 @@ namespace BossesPoisonAura
             }
             
             base.AI(npc);
+        }
+
+        public override void DrawEffects(NPC npc, ref Color drawColor)
+        {
+            //check if the npc is a boss
+            if (ffFunc.IsNPCaBoss(npc))
+            {
+                //define how much dust for the radius
+                int radiusDustAmount = 30;
+
+                //create the dusts needed
+                for (int i = 0; i < radiusDustAmount; i++)
+                {
+                    //set the inital offset of the dust
+                    Vector2 offset = new Vector2(radius, 0);
+
+                    //rotate the offset
+                    int rotateBy = 360 / radiusDustAmount * i;
+                    offset = offset.RotatedBy(MathHelper.ToRadians(rotateBy));
+
+                    //create the dust
+                    Dust.NewDustPerfect(npc.position + offset, DustID.Grass);
+                }
+            }
+            base.DrawEffects(npc, ref drawColor);
         }
     }
 }
