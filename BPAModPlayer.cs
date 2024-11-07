2ef184b17de12e7ad3cb17c65e55705c95da1b2d
First Fractal <firstfractal512@gmail.com>
2024-11-06 20:18:35 -0600

    fixed a bug where poison is constanly going down, and buff inital poison damage

--- BPAModPlayer.cs
+++ BPAModPlayer.cs
@@ -7,7 +7,7 @@ namespace BossesPoisonAura
     {
         //variables for if the blue poison is active and the amount of damage it does
         public bool bluePoison = false;
-        public int intialPoison = 10;
+        public int intialPoison = 28;
 
         //reset the blue poison to be false when not active
         public override void ResetEffects()
@@ -30,6 +30,8 @@ namespace BossesPoisonAura
             {
                 //check how much poison nerf upgrades the world has
                 double poisonNerf = 0;
+                int poisonAmount = 0;
+
                 foreach(int upgrade in ModContent.GetInstance<BPAModSystem>().upgrades)
                 {
                     //if the current upgrade tier is the poison one and counter items are allowed, then decrease the radius
@@ -40,10 +42,10 @@ namespace BossesPoisonAura
                 if (Player.lifeRegen > 0) Player.lifeRegen = 0;
                 
                 //nerf the posion if there is any upgrades for it
-                if (poisonNerf > 0) intialPoison = (int)(intialPoison * Math.Pow(0.66, poisonNerf));
+                if (poisonNerf > 0) poisonAmount = (int)(intialPoison * Math.Pow(0.66, poisonNerf));
 
                 //make it lose hp
-                Player.lifeRegen -= intialPoison;
+                Player.lifeRegen -= poisonAmount;
             }
             base.UpdateBadLifeRegen();
         }
